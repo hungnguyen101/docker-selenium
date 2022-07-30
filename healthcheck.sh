d4d52820d3dd551c1167b5dd433689efe9a82078
hungnguyen101 <nguyenthehung93sgu@gmail.com>
2022-07-30 18:30:23 +0700

    add update pipeline

--- healthcheck.sh
+++ healthcheck.sh
@@ -16,3 +16,5 @@ java -cp selenium-docker.jar:selenium-docker-tests.jar:libs/* \
     -DHUB_HOST=$HUB_HOST \
     -DBROWSER=$BROWSER \
     org.testng.TestNG $MODULE
+echo "check folder inside container"
+ls -al
